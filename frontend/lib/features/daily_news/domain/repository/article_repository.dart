9e755a93f9e52f762dc09a0288d62fc6efb9f4fe
Pedro-kt <bustamante.dev.mobile@gmail.com>
2026-09-23 17:31:56 -0300

    feat(domain): extend article repository interface with upload methods
    - Add uploadArticle(CreateArticleParams): upload new article with thumbnail
    - Add updateArticle(UpdateArticleParams): modify existing article
    - Add deleteArticle(DeleteArticleParams): remove user article
    - Add getUserArticles(GetUserArticlesParams): fetch user's articles with pagination
    - Add updateArticlePublishStatus(UpdatePublishStatusParams): toggle publish status

--- frontend/lib/features/daily_news/domain/repository/article_repository.dart
+++ frontend/lib/features/daily_news/domain/repository/article_repository.dart
@@ -1,14 +1,30 @@
 import 'package:news_app_clean_architecture/core/resources/data_state.dart';
 import 'package:news_app_clean_architecture/features/daily_news/domain/entities/article.dart';
+import 'package:news_app_clean_architecture/features/daily_news/domain/entities/article_upload_params.dart';
 
 abstract class ArticleRepository {
   // API methods
   Future<DataState<List<ArticleEntity>>> getNewsArticles();
 
-  // Database methods
-  Future < List < ArticleEntity >> getSavedArticles();
+  // Database methods (saved articles)
+  Future<List<ArticleEntity>> getSavedArticles();
 
-  Future < void > saveArticle(ArticleEntity article);
+  Future<void> saveArticle(ArticleEntity article);
 
-  Future < void > removeArticle(ArticleEntity article);
+  Future<void> removeArticle(ArticleEntity article);
+
+  // Article upload methods
+  Future<DataState<ArticleEntity>> uploadArticle(CreateArticleParams params);
+
+  Future<DataState<ArticleEntity>> updateArticle(UpdateArticleParams params);
+
+  Future<DataState<void>> deleteArticle(DeleteArticleParams params);
+
+  Future<DataState<List<ArticleEntity>>> getUserArticles(
+    GetUserArticlesParams params,
+  );
+
+  Future<DataState<void>> updateArticlePublishStatus(
+    UpdatePublishStatusParams params,
+  );
 }
